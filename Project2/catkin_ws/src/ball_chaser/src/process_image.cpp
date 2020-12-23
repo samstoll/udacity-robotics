7166a961091d15e9354870ba6997f74f76e5adf1
Sam Stoll <samstoll1@gmail.com>
2020-12-23 02:33:00 +0000

    fix comments

--- Project2/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ Project2/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -8,7 +8,7 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    // Request a service and pass the velocities to it to drive the robot
     ball_chaser::DriveToTarget srv;
     srv.request.linear_x = lin_x;
     srv.request.angular_z = ang_z;
@@ -23,7 +23,7 @@ void process_image_callback(const sensor_msgs::Image img)
 
     int white_pixel = 255;
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
+    // Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
